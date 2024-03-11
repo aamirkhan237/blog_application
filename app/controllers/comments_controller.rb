d479ead8fab028a8b949dec4c32e88d8e841eaa3
aamirkhan237 <aamir.4u71@gmail.com>
2024-03-11 15:48:24 +0530

    Notification Functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,45 +1,43 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_post
+  
     def create
-        
-        @comment =@post.comments.create(comment_params)
-        @comment.user = current_user
-
-        if @comment.save
-            flash[:notice]="Comment has been created"
-            redirect_to post_path(@post)
-        else @comment.save
-            flash[:alert]="Comment has not been created"
-            redirect_to post_path(@post)
-        end
+      @comment = @post.comments.create(comment_params)
+      @comment.user = current_user
+  
+      if @comment.save
+        redirect_to post_path(@post), notice: 'Comment has been created'
+      else
+        redirect_to post_path(@post), alert: 'Comment has not been created'
+      end
     end
-
+  
     def destroy
-        @comment=@post.comments.find(params[:id])
-        @comment.destroy
-        redirect_to post_path(@post)
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to post_path(@post)
     end
-
+  
     def update
-        @comment=@post.comments.find(params[:id])
-
-        respond_to do |format|
-            if @comment.update(comment_params)
-                format.html {redirect_to post_url (@post), notice: "Comment has been Updated"}
-            else 
-                format.html {redirect_to post_url (@post), alert: "Comment was not  Updated"}
-            end
+      @comment = @post.comments.find(params[:id])
+  
+      respond_to do |format|
+        if @comment.update(comment_params)
+          format.html { redirect_to post_url(@post), notice: 'Comment has been updated' }
+        else
+          format.html { redirect_to post_url(@post), alert: 'Comment was not updated!' }
         end
+      end
     end
-
+  
     private
+  
     def set_post
-        @post =Post.find(params[:post_id])
+      @post = Post.find(params[:post_id])
     end
-
+  
     def comment_params
-        params.require(:comment).permit(:body)
-end
-
-end
+      params.require(:comment).permit(:body)
+    end
+  end
