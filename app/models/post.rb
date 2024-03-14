d9ec7eeef5a6e045e46062e3b6d1440c13952f71
aamirkhan237 <aamir.4u71@gmail.com>
2024-03-14 12:25:44 +0530

    Admin Dashboard for search

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,13 @@ class Post < ApplicationRecord
     validates :title , presence: true ,length:{minimum:5, maximum:50}
     validates :body , presence: true ,length:{minimum:10, maximum:100}
     belongs_to :user
-
     has_many :comments, dependent: :destroy
+
+    has_rich_text :body
+
+
     has_noticed_notifications model_name: 'Notification'
-    has_many :notifications, through: :user, dependent: :destroy
+    has_many :notifications, through: :user
         # Other model code...
       
         def self.ransackable_attributes(auth_object = nil)
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,29 @@
 Rails.application.routes.draw do
   get 'search', to: 'search#index'
+
+  authenticated :user, ->(u) { u.admin? } do
+    get 'admin/', to: 'admin#index'
+    get 'admin/posts'
+    get 'admin/comments'
+    get 'admin/users'
+    get 'admin/post/:id', to: 'admin#show_post', as: 'admin_post'
+  end
+
   get 'users/profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  get '/u/:id', to: 'users#profile', as: 'user'
 
-  get "/u/:id", to: "users#profile" , as: 'user'
-
-#/posts/1/comments/4
-
+  # /posts/1/comments/4
   resources :posts do
     resources :comments
   end
 
-
-  get 'home', to: "pages#home" 
-  get 'about', to: "pages#about"
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and  uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
-  root "pages#home"
+  root 'pages#home'
 end
